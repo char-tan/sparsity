1ec153ae3c52fdc9a2a193e25bf5553578d33177
DSep <dyanatkar@me.com>
2022-12-29 10:50:14 -0800

    Add pybind bindings for our functions

--- sparse_ops/test.cpp
+++ sparse_ops/test.cpp
@@ -6,3 +6,7 @@ torch::Tensor add(torch::Tensor a, torch::Tensor b) {
   torch::Tensor c = a + b;
   return c;
 }
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def("add", &add, "Sparse Add");
+}
