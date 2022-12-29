d0253cb7c1c72d49741480f4d9c64f54b18a003a
char-tan <hello.spam@tutanota.com>
2022-12-29 23:57:34 +0000

    tried doing add with kernel

--- ops/ops_cuda_kernel.cu
+++ ops/ops_cuda_kernel.cu
@@ -4,10 +4,29 @@
 #include <cuda_runtime.h>
 
 
+template <typename scalar_t>
+__global__ void cuda_add_kernel(
+    scalar_t* __restrict__ a,
+    scalar_t* __restrict__ b,
+    scalar_t* __restrict__ c
+    ) {
+        c = a + b
+      }
+
+
 torch::Tensor cuda_add(
 		torch::Tensor a,
 		torch::Tensor b)
 {
-	auto c = torch::sum(a, b)
-	return {c};
+
+  auto c = torch::zeros_like(a);
+
+  AT_DISPATCH_FLOATING_TYPES(a.type(), "cuda_add", ([&] {
+    cuda_add_kernel<scalar_t><<<1, 1>>>(
+        a.data<scalar_t>(),
+        b.data<scalar_t>(),
+        c.data<scalar_t>(),
+  })));
+
+  return {c}
 }
