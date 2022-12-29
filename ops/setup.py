e65af1b13b228aea6fada74fe3119d681c28713d
char-tan <hello.spam@tutanota.com>
2022-12-29 20:56:04 +0000

    fix setup.py, now having import error

--- ops/setup.py
+++ ops/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
 setup(name='ops_cpp',
-      ext_modules=[cpp_extension.CppExtension('ops_cpp', ['add.cpp', 'ops.cpp'])],
+      ext_modules=[cpp_extension.CppExtension('ops_cpp', ['ops_cuda.cpp'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
