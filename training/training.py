a2f847fac3579f81759d7504e87a4727b0eaff21
char-tan <hello.spam@tutanota.com>
2023-01-12 01:35:29 +0000

    refactor training code, new notebook for sparse training

--- training/training.py
+++ training/training.py
@@ -1,15 +1,11 @@
-# from apex.contrib.sparsity import ASP
-# ASP.prune_trained_model(model, optimizer) #pruned a trained model
-
 import numpy as np
+import time as t
 
 import torch
 import torch.nn.functional as F
 
-import torchvision
-import torchvision.transforms as T
-
-import time as t
+from training.utils import cifar10_dataloaders
+from training.utils import epoch_summary
 
 
 def train_epoch(model, train_loader, optimizer, scheduler, device, verbose=0):
@@ -66,99 +62,24 @@ def test_epoch(model, test_loader, device, verbose=0):
     return np.mean(test_loss), np.mean(test_acc)
 
 
-def print_info(info: dict):
-
-    statement = ""
-
-    for key, val in info.items():
-
-        statement += "{0}: {1:.4g} | ".format(key, val)
-
-    print(statement)
-
-
-def train_model(model):
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    n_epochs = 50
-    batch_size = 256
-
-    momentum = 0.9
-    lr = 0.1
-    weight_decay = 5e-4
-    # seed?
-
-    verbose = False
-
-    model = model.to(device)
-
-    train_transforms = T.Compose(
-        [
-            T.RandomCrop(32, padding=4),
-            T.RandomHorizontalFlip(),
-            T.ToTensor(),
-            T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-            
-            
-        ]
-    )
-
-    test_transforms = T.Compose(
-        [T.ToTensor(), T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]
-    )
-
-    train_kwargs = {
-        "root": "data",
-        "train": True,
-        "download": True,
-        "transform": train_transforms,
-    }
-    test_kwargs = {
-        "root": "data",
-        "train": False,
-        "download": False,
-        "transform": test_transforms,
-    }
-
-    train_set = torchvision.datasets.CIFAR10(**train_kwargs)
-    test_set = torchvision.datasets.CIFAR10(**test_kwargs)
-
-    train_loader = torch.utils.data.DataLoader(
-        train_set, batch_size=batch_size, shuffle=True, num_workers=2, pin_memory=True
-    )
-
-    test_loader = torch.utils.data.DataLoader(
-        test_set,
-        batch_size=batch_size * 4,
-        shuffle=False,
-        num_workers=2,
-        pin_memory=True,
-    )
-
-    optimizer = torch.optim.SGD(
-        model.parameters(),
-        lr=lr,
-        momentum=momentum,
-        weight_decay=weight_decay,
-    )
+def train_phase(model, optimizer, train_loader, test_loader, config):
 
     scheduler = torch.optim.lr_scheduler.OneCycleLR(
-        optimizer,
-        max_lr=lr,
+    optimizer,
+        max_lr=config.lr,
         div_factor=1e5,
-        epochs=n_epochs,
+        epochs=config.num_epochs,
         steps_per_epoch=len(train_loader),
-        pct_start=1 / n_epochs,
-        verbose=verbose,
+        pct_start=1 / config.num_epochs,
     )
 
     time = t.time()
 
-    for epoch in range(n_epochs):
+    for epoch in range(config.num_epochs):
         train_loss, train_acc = train_epoch(
-            model.train(), train_loader, optimizer, scheduler, device
+            model.train(), train_loader, optimizer, scheduler, config.device
         )
-        test_loss, test_acc = test_epoch(model.eval(), test_loader, device)
+        test_loss, test_acc = test_epoch(model.eval(), test_loader, config.device)
 
         info = {
             "epoch": epoch,
@@ -169,10 +90,5 @@ def train_model(model):
             "test acc": test_acc,
         }
 
-        print_info(info)
+        epoch_summary(info)
         time = t.time()
-
-
-if __name__ == "__main__":
-    train_model()
-
---
+++ training/utils.py
@@ -0,0 +1,93 @@
+from dataclasses import dataclass
+
+import torch
+
+import torchvision
+import torchvision.transforms as T
+
+
+def mask_checkpoint(checkpoint, masked_model):
+
+    for key, value in masked_model.state_dict().items():
+        if '.__weight_mma_mask' in key:
+            layer_name = key.replace('.__weight_mma_mask', '.weight')
+            checkpoint[layer_name] *= value
+
+    return checkpoint
+
+@dataclass
+class Config:
+    device: str = "cuda" if torch.cuda.is_available() else "cpu"
+    num_epochs: int = 50
+    batch_size: int = 256
+    momentum: float = 0.9
+    lr: float = 0.1
+    weight_decay: float = 5e-4
+    seed: int = 42
+
+
+def cifar10_dataloaders(config):
+
+    train_transforms = T.Compose(
+        [
+            T.RandomCrop(32, padding=4),
+            T.RandomHorizontalFlip(),
+            T.ToTensor(),
+            T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ]
+    )
+
+    test_transforms = T.Compose(
+        [T.ToTensor(), T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]
+    )
+
+    train_kwargs = {
+        "root": "data",
+        "train": True,
+        "download": True,
+        "transform": train_transforms,
+    }
+    test_kwargs = {
+        "root": "data",
+        "train": False,
+        "download": False,
+        "transform": test_transforms,
+    }
+
+    train_set = torchvision.datasets.CIFAR10(**train_kwargs)
+    test_set = torchvision.datasets.CIFAR10(**test_kwargs)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=config.batch_size, shuffle=True, num_workers=2, pin_memory=True
+    )
+
+    test_loader = torch.utils.data.DataLoader(
+        test_set,
+        batch_size=config.batch_size * 4,
+        shuffle=False,
+        num_workers=2,
+        pin_memory=True,
+    )
+
+    return train_loader, test_loader
+
+
+def resnet18_small_input():
+
+    # init model and change first layers for smaller input images
+    model = torchvision.models.resnet18(num_classes=10, zero_init_residual=True)
+    model.conv1 = torch.nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+    model.maxpool = torch.nn.Identity()
+
+    return model
+
+
+def epoch_summary(info: dict):
+
+    summary = ""
+
+    for key, val in info.items():
+
+        summary += "{0}: {1:.4g} | ".format(key, val)
+
+    print(summary)
