e519ff0ff037f6a585b533ad3e3d05a8ecf1aecd
DSep <dyanatkar@me.com>
2023-01-15 16:44:11 +0000

    Added install from source and install via cached wheel notebooks

--- ops/install.sh
+++ ops/install.sh
@@ -2,16 +2,18 @@
 
 # Download and setup the cuSPARSELT library
 
-wget https://developer.download.nvidia.com/compute/cusparselt/redist/libcusparse-lt/linux-x86_64/libcusparse_lt-linux-x86_64-0.3.0.3-archive.tar.xz
-tar -xf libcusparse_lt-linux-x86_64-0.3.0.3-archive.tar.xz
-mv libcusparse_lt-linux-x86_64-0.3.0.3-archive /content
-# export CUSPARSELT_DIR=/content/libcusparse_lt-linux-x86_64-0.3.0.3-archive
-# export LD_LIBRARY_PATH=${CUSPARSELT_DIR}/lib64:${LD_LIBRARY_PATH}
+# pip3 uninstall cupy-cuda11x
 
-# 
+# wget https://developer.download.nvidia.com/compute/cusparselt/redist/libcusparse-lt/linux-x86_64/libcusparse_lt-linux-x86_64-0.3.0.3-archive.tar.xz
+# tar -xf libcusparse_lt-linux-x86_64-0.3.0.3-archive.tar.xz
+# cp -T libcusparse_lt-linux-x86_64-0.3.0.3-archive /content/libcusparse_lt-linux-x86_64-0.3.0.3-archive 
+cp /content/libcusparse_lt-linux-x86_64-0.3.0.3-archive/lib/libcusparseLt* /usr/local/cuda-11.2/targets/x86_64-linux/lib
+cp /content/libcusparse_lt-linux-x86_64-0.3.0.3-archive/lib/libcusparseLt.so /usr/local/cuda-11.2/targets/x86_64-linux/lib/stubs
+cp /content/libcusparse_lt-linux-x86_64-0.3.0.3-archive/include/cusparseLt.h /usr/local/cuda-11.2/include
+
 export CUSPARSELT_DIR=/content/libcusparse_lt-linux-x86_64-0.3.0.3-archive
 export CUSPARSELT_PATH=/content/libcusparse_lt-linux-x86_64-0.3.0.3-archive
-export LD_LIBRARY_PATH=/content/libcusparse_lt-linux-x86_64-0.3.0.3-archive/lib64:/usr/lib64-nvidia:/content/libcusparse_lt-linux-x86_64-0.3.0.3-archive/lib/
+export LD_LIBRARY_PATH=/content/libcusparse_lt-linux-x86_64-0.3.0.3-archive/lib:/usr/lib64-nvidia # :/content/libcusparse_lt-linux-x86_64-0.3.0.3-archive/lib/
 # export CUDA_TOOLKIT=$(dirname $(realpath $(which nvcc))/..)
 export CUDA_TOOLKIT=/usr/local/cuda-11.2 # HACK to replace line above
 echo "Important variables:"
@@ -21,6 +23,10 @@ echo $LD_LIBRARY_PATH
 echo $CUDA_TOOLKIT
 ldconfig
 
+CUDA_PATH=/usr/local/cuda-11.2
+# pip3 install cupy-cuda112
+
+
 # Install our PyTorch extension
 python setup.py install
-# pip3 install .
+pip3 install .
--- ops/ops_cuda_kernel.cu
+++ ops/ops_cuda_kernel.cu
@@ -2,15 +2,113 @@
 
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <cusparseLt.h> // cusparseLt header
+
+
+// template <typename scalar_t>
+// __global__ void cuda_add_kernel(
+//     scalar_t* __restrict__ a,
+//     scalar_t* __restrict__ b,
+//     scalar_t* __restrict__ c
+//     ) {
+//         auto c = torch::add(a, b);
+// 	    return c;
+//       }
 
 
 template <typename scalar_t>
-__global__ void cuda_add_kernel(
+__global__ void cuda_sparse_mm_kernel(
     scalar_t* __restrict__ a,
     scalar_t* __restrict__ b,
     scalar_t* __restrict__ c
     ) {
-        c = a + b
+
+        // Device pointers and coefficient definitions
+        float alpha = 1.0f;
+        float beta  = 0.0f;
+        __half* dA = ...
+        __half* dB = ...
+        __half* dC = ...
+
+        //--------------------------------------------------------------------------
+        // cusparseLt data structures and handle initialization
+        cusparseLtHandle_t             handle;
+        cusparseLtMatDescriptor_t      matA, matB, matC;
+        cusparseLtMatmulDescriptor_t   matmul;
+        cusparseLtMatmulAlgSelection_t alg_sel;
+        cusparseLtMatmulPlan_t         plan;
+        cudaStream_t                   stream = nullptr;
+        cusparseLtInit(&handle);
+
+        //--------------------------------------------------------------------------
+        // matrix descriptor initialization
+        cusparseLtStructuredDescriptorInit(&handle, &matA, num_A_rows, num_A_cols,
+                                          lda, alignment, type, order,
+                                          CUSPARSELT_SPARSITY_50_PERCENT);
+        cusparseLtDenseDescriptorInit(&handle, &matB, num_B_rows, num_B_cols, ldb,
+                                      alignment, type, order);
+        cusparseLtDenseDescriptorInit(&handle, &matC, num_C_rows, num_C_cols, ldc,
+                                      alignment, type, order);
+
+        //--------------------------------------------------------------------------
+        // matmul, algorithm selection, and plan initialization
+        cusparseLtMatmulDescriptorInit(&handle, &matmul, opA, opB, &matA, &matB,
+                                      &matC, &matC, compute_type);
+        cusparseLtMatmulAlgSelectionInit(&handle, &alg_sel, &matmul,
+                                        CUSPARSELT_MATMUL_ALG_DEFAULT);
+        cusparseLtMatmulPlanInit(&handle, &plan, &matmul, &alg_sel, workspace_size);
+
+        //--------------------------------------------------------------------------
+        // Prune the A matrix (in-place) and check the correctness
+        cusparseLtSpMMAPrune(&handle, &matmul, dA, dA, CUSPARSELT_PRUNE_SPMMA_TILE,
+                            stream);
+        int *d_valid;
+        cudaMalloc((void**) &d_valid, sizeof(d_valid));
+        cusparseLtSpMMAPruneCheck(&handle, &matmul, dA, &d_valid, stream);
+
+        int is_valid;
+        cudaMemcpyAsync(&is_valid, d_valid, sizeof(d_valid), cudaMemcpyDeviceToHost,
+                        stream);
+        cudaStreamSynchronize(stream);
+        if (is_valid != 0) {
+            std::printf("!!!! The matrix has been pruned in a wrong way. "
+                        "cusparseLtMatmul will not provided correct results\n");
+            return EXIT_FAILURE;
+        }
+
+        //--------------------------------------------------------------------------
+        // Matrix A compression
+        size_t compressed_size;
+        cusparseLtSpMMACompressedSize(&handle, &plan, &compressed_size);
+        cudaMalloc((void**) &dA_compressed, compressed_size);
+
+        cusparseLtSpMMACompress(&handle, &plan, dA, dA_compressed, stream);
+
+        //--------------------------------------------------------------------------
+        // Allocate workspace
+        size_t workspace_size;
+        void*  d_workspace = nullptr;
+
+        cusparseLtMatmulGetWorkspace(&handle, &plan, &workspace_size);
+        cudaMalloc((void**) &d_workspace, workspace_size);
+
+        //--------------------------------------------------------------------------
+        // Perform the matrix multiplication
+        int           num_streams = 0;
+        cudaStream_t* streams     = nullptr;
+
+        cusparseLtMatmul(&handle, &plan, &alpha, dA_compressed, dB, &beta, dC, dD,
+                        d_workspace, streams, num_streams)
+
+        //--------------------------------------------------------------------------
+        // Destroy descriptors, plan and handle
+        cusparseLtMatDescriptorDestroy(&matA);
+        cusparseLtMatDescriptorDestroy(&matB);
+        cusparseLtMatDescriptorDestroy(&matC);
+        cusparseLtMatmulPlanDestroy(&plan);
+        cusparseLtDestroy(&handle);
+
+        c = a
       }
 
 
@@ -19,14 +117,16 @@ torch::Tensor cuda_add(
 		torch::Tensor b)
 {
 
-  auto c = torch::zeros_like(a);
+    auto c = torch::add(a, b);
+    return c;
+}
 
-  AT_DISPATCH_FLOATING_TYPES(a.type(), "cuda_add", ([&] {
-    cuda_add_kernel<scalar_t><<<1, 1>>>(
-        a.data<scalar_t>(),
-        b.data<scalar_t>(),
-        c.data<scalar_t>(),
-  })));
+torch::Tensor cuda_sparse_mm(
+		torch::Tensor a,
+		torch::Tensor b)
+{
 
-  return {c}
+  // auto c = torch::zeros_like(a);
+    auto c = torch::add(a, b);
+    return c;
 }
