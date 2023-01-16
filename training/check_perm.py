9c78677edf1eb2fb4ab4e82ea14b8fba99387f04
char-tan <hello.spam@tutanota.com>
2023-01-16 17:04:46 +0000

    perm checking runs

--- training/check_perm.py
+++ training/check_perm.py
@@ -13,15 +13,15 @@ def seek_row_perm(p, p_pruned, mask):
     n_rows = p.shape[0]
 
     maxes = torch.zeros(n_rows)
-    argmaxes = torch.zeros(n_rows)
+    argmaxes = torch.zeros(n_rows, dtype=int)
 
     for i in range(n_rows):
+
+        print(i)
 
         eqs = torch.zeros(n_rows)
 
         for j in range(n_rows):
-
-            print(i, j)
 
             args = torch.argwhere(mask[j]).flatten()
             eqs[j] = torch.eq(p[i][args], p_pruned[j][args]).sum()
@@ -33,9 +33,6 @@ def seek_row_perm(p, p_pruned, mask):
 
 
 def find_layer_perm(p, p_pruned, mask):
-
-    print(p.shape)
-    breakpoint()
 
     assert p.shape == p_pruned.shape
 
@@ -50,15 +47,46 @@ def find_layer_perm(p, p_pruned, mask):
     r_maxes, r_arg_maxes = seek_row_perm(p, p_pruned, mask)
     c_maxes, c_arg_maxes = seek_row_perm(p.t(), p_pruned.t(), mask.t())
 
-    if torch.max(r_maxes / r_len) > torch.max(c_maxes / c_len):
-        return r_arg_maxes, 'R'
+    r_normalised_unmatched = (r_len - r_maxes.mean()) / r_len
+    c_normalised_unmatched = (c_len - c_maxes.mean()) / c_len
+
+    if r_normalised_unmatched < c_normalised_unmatched:
+        perm = r_arg_maxes.long(), 'R'
+    elif r_normalised_unmatched > c_normalised_unmatched:
+        perm = c_arg_maxes.long(), 'C'
+    elif torch.count_nonzero(mask) == mask.numel():
+        # mask is all ones (no mask), can return either
+        perm = r_arg_maxes.long(), 'R'
     else:
-        return c_arg_maxes, 'C'
+        print('problem!')
+        assert ValueError
+
+    return perm
+
+
+def apply_layer_perm(p, perm):
+
+    orig_shape = p.shape
+
+    p = p.view(orig_shape[0], -1)
+
+    #print(p.shape)
+    #print(perm[0].shape)
+
+    if perm[-1] == 'R':
+        p = p[perm[0],:]
+    elif perm[-1] == 'C':
+        p = p[:,perm[0]]
+    else:
+        print('problem!')
+        assert ValueError
+
+    p = p.view(orig_shape)
+
+    return p
+
 
 torch.set_printoptions(linewidth=250)
-
-#phase1 = resnet18_small_input()
-#phase1_pruned = resnet18_small_input()
 
 phase1 = MyFirstCNN()
 phase1_pruned = MyFirstCNN()
@@ -69,6 +97,8 @@ weight1_pruned = torch.load("phase1_pruned_mf.pt", map_location="cpu")
 phase1.load_state_dict(weight1)
 phase1_pruned.load_state_dict(weight1_pruned, strict=False)
 
+#print(weight1_pruned.keys())
+
 layer_perms = dict()
 
 with torch.no_grad():
@@ -76,37 +106,55 @@ with torch.no_grad():
         phase1.named_parameters(), phase1_pruned.named_parameters()
     ):
 
-        print(n)
-        
-        if n_pruned.replace('.weight', '.__weight_mma_mask') in weight1_pruned.keys():
-            mask = weight1_pruned[n_pruned.replace('.weight', '.__weight_mma_mask')]
-        else:
-            mask = torch.ones_like(p)
+        print('\nseeking permutaion for', n)
 
-        if "conv" in n:
-            perm = find_layer_perm(p, p_pruned, mask)
+        if 'weight' in n:
 
-        elif "bn" in n:
-            # batch norm has same permutation as previous layer
-            perm = perm
+            # check if there is a weight mask, if not mask all ones
+            if n.replace('.weight', '.__weight_mma_mask') in weight1_pruned.keys():
+                mask = weight1_pruned[n_pruned.replace('.weight', '.__weight_mma_mask')]
+            else:
+                mask = torch.ones_like(p)
 
-        elif "downsample" in n:
-            perm = torch.arange(p.shape[0])
+            if "conv" in n: 
+                perm = find_layer_perm(p, p_pruned, mask)
 
-        elif "fc.weight" in n:
-            perm = find_layer_perm(p, p_pruned)
+            elif 'linear' in n:
+                # TODO - can't handle permuted linear layers
+                perm = torch.arange(p.shape[0]), 'R'
 
-        elif "fc.bias" in n:
-            perm = perm
+            elif "bn" in n:
+                # batch norm has same permutation as previous layer
+                perm = perm
+            
+            elif "downsample" in n:
+                # TODO handle DS - think is just same as conv
+                perm = find_layer_perm(p, p_pruned, mask)
+                breakpoint()
 
-        elif 'linear' in n:
-            perm = find_layer_perm(p, p_pruned, mask)
+        elif "bias" in n:
+            if perm[-1] == 'R':
+                # bias has same permutation as previous layer
+                perm = perm
+            else:
+                perm = torch.arange(len(p)), 'R'
+
         else:
             raise ValueError
 
         layer_perms[n] = perm
 
-    print(layer_perms)
+#print(layer_perms)
 
-# for k, v in layer_perms.items():
-#    print(k, v)
+for n, p in phase1.named_parameters():
+
+    print('\npermuting', n)
+
+    old_p = p.clone()
+    perm = layer_perms[n]
+    p = apply_layer_perm(p, perm)
+
+    assert p.shape == old_p.shape
+    print(torch.eq(old_p, p).sum(), p.numel())
+
+breakpoint()
